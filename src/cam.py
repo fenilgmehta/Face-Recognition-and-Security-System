9d80115f18639938823b04583d38bb65bcb5b229
Fenil Mehta <fenilgmehta@gmail.com>
2019-02-21 19:12:32 +0530

    "cam.py" file indentation changed from tabs to spaces.

--- src/cam.py
+++ src/cam.py
@@ -1,29 +1,29 @@
 import cv2
 import os
 
-def capture(path, mode):						# path for storing image; mode to give title to capture window 
+def capture(path, mode):                        # path for storing image; mode to give title to capture window 
 
-	camera = cv2.VideoCapture(0)							# select defualt camera i.e 0
+    camera = cv2.VideoCapture(0)                            # select defualt camera i.e 0
 
-	path, dirs, files = next(os.walk(path))
-	img_counter = len(files)							#count files in directory
+    path, dirs, files = next(os.walk(path))
+    img_counter = len(files)                            #count files in directory
 
-	while True:
-		ret, frame = camera.read()
-		cv2.imshow(mode, frame)
-		if not ret:
-			break
-		k = cv2.waitKey(1)
+    while True:
+        ret, frame = camera.read()
+        cv2.imshow(mode, frame)
+        if not ret:
+            break
+        k = cv2.waitKey(1)
 
-		if k%256 == 27:								# ESC pressed
-			print("Escape, closing capture...")
-			break
+        if k%256 == 27:                             # ESC pressed
+            print("Escape, closing capture...")
+            break
 
-		elif k%256 == 13:								# Enter pressed (for spacebar event '32')
-			img_name = path+"image_{}.png".format(img_counter)		#location to store image
-			cv2.imwrite(img_name, frame)
-			print("{} saved!".format(img_name))
-			img_counter += 1
+        elif k%256 == 13:                               # Enter pressed (for spacebar event '32')
+            img_name = path+"image_{}.png".format(img_counter)      #location to store image
+            cv2.imwrite(img_name, frame)
+            print("{} saved!".format(img_name))
+            img_counter += 1
 
-	camera.release()
-	cv2.destroyAllWindows()
+    camera.release()
+    cv2.destroyAllWindows()
