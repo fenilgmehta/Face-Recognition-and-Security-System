286e89938afa5f5ba7eea574fb2bfbb427885c2c
Fenil Mehta <fenilgmehta@gmail.com>
2019-03-01 15:46:20 +0530

    cam_pi.py: variable rename and imports truncated

--- src/cam_pi.py
+++ src/cam_pi.py
@@ -1,16 +1,15 @@
 # python 2
 from picamera import PiCamera
 from time import sleep
-import os
-import sender
+from os import walk
 
 class Camera:
     def __init__(self):
         # start PiCamera
         self.camera = PiCamera()
     
-    def capture(self, path = './face_testing/unknown_pictures/', pic_name = 'image_', pic_delay = 3):
-        path, dirs, files = next(os.walk(path))
+    def capture(self, path = './face_testing/unknown_pictures/', pic_name_prefix = 'image_', pic_delay = 3):
+        path, dirs, files = next(walk(path))
         
         #count files in directory
         img_counter = len(files)
@@ -20,7 +19,7 @@ class Camera:
         # wait for pic_delay and take the photo
         sleep(pic_delay)
         # take the photo and save it with the name "image_[img_counter+1].png"
-        self.camera.capture(path+pic_name+"{}.png".format(img_counter+1))
+        self.camera.capture(path+pic_name_prefix+"{}.png".format(img_counter+1))
         # close PiCamera
         self.camera.stop_preview()
         
