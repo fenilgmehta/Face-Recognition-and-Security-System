bff264ca708b6050f5e60d39572a354fe2d560ef
Fenil Mehta <fenilgmehta@gmail.com>
2019-03-17 15:23:14 +0530

    main_door.py: complete revamp with bug fixes

--- src/main_door.py
+++ src/main_door.py
@@ -1,19 +1,42 @@
-# this python program is to be used at the enterance
-# of the home where this IoT system is implemented.
+# python 2/3
+# Description: this python program is to be used at the enterance of the home where this Internet of Things(IoT) system is implemented
 
-import sys
-import argparse
-from cam import capture
-from face_recognition_module import faceRecognition
-print("\n### import complete ###\n")
+print("=== import started (main_door) ===")
 
-def main():
-    print("Capturing image for recognition (Hit Enter to take a snap and Ecs to stop.)")
+try:
+    from cam_pi import Camera
+    is_raspberry_pi = True
+except ImportError:
+    from cam_laptop import Camera
+    is_raspberry_pi = False
+
+try:
+    input = raw_input
+except NameError:
+    pass
+
+from face_recognizer import FaceRecognizer
+
+print("=== import complete (main_door) ===\n")
+
+
+def authorize_guests():
+    fr = FaceRecognizer("./z_face_testing/pictures_of_people_i_know/")
+    fr.train_on_folder_tree(True)
+    fr.save_to_file()
+
+    print("\nWaiting for a guest (press \"ENTER\" to take a photo of the guest and \"exit\" to stop)")
     while(True):
-        if raw_input()=="exit": return
-        capture("image", "./face_testing/unknown_pictures/","Face Recognition")     # capture image for recognition
-        faceRecognition()                           # Recognize face
+        if input()=="exit": return
+
+        # capture image for training
+        images_path_tuple = Camera.capture_single_image("./z_face_testing/unknown_pictures/")
+
+        # do face recognition and return the results
+        # here is have directly accessed the element at 0th index as each time only one photo is taken
+        results = fr.face_detection(images_path_tuple[0])
+        print(results)
 
 
 if __name__ == '__main__':
-    main()
+    authorize_guests()
