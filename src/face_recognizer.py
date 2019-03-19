6cb6d9959b211621a464a1ebc72f59bf49d84bb5
Fenil Mehta <fenilgmehta@gmail.com>
2019-03-19 18:41:51 +0530

    face_recognizer.py: two new methods added and a small change in "train_on_folder_tree" method
    Method 1 - "retrain_on_folder" -  to retrain an individuals face data-set
    Method 2 - "train_on_image" - add individual image to known faces data-set

--- src/face_recognizer.py
+++ src/face_recognizer.py
@@ -46,7 +46,6 @@ class FaceRecognizer:
         '''
 
         folder_path = self.path_to_training_data
-        folder_path = os.path.abspath(folder_path)
         if not os.path.exists(folder_path): return
 
         known_faces = []
@@ -55,6 +54,17 @@ class FaceRecognizer:
             res = self.__train_on_folder(folder_path + "/" + i, delete_image_without_faces)
             if len(res[1]) == 0: continue
             self.known_faces_encoded.append(res)
+
+
+    def retrain_on_folder(self, person_name, delete_image_without_faces = False):
+        # person_name = folder_path[folder_path.rfind("/")+1:]
+        folder_path = self.path_to_training_data + "/" + person_name
+        if self.known_faces_encoded.count(person_name) > 0:
+            index_to_remove = self.known_faces_encoded.index(person_name)
+            self.known_faces_encoded.pop(index_to_remove)
+
+        res = self.__train_on_folder(folder_path, delete_image_without_faces)
+        if len(res[1]) != 0: self.known_faces_encoded.append(res)
 
 
     def __train_on_folder(self, folder_path, delete_image_without_faces = False):
@@ -93,6 +103,27 @@ class FaceRecognizer:
                 print("message: added new image file \"" + file_path + "\"")
                 pic_list_encoded.extend(known_picture_encoded)
         return (person_name, pic_list_encoded)
+
+
+    def train_on_image(self, person_name, image_path, delete_image_without_faces = False):
+        index_to_insert = len(self.known_faces_encoded)
+        if self.known_faces_encoded.count(person_name) > 0:
+            index_to_insert = self.known_faces_encoded.index(person_name)
+        else:
+            self.known_faces_encoded.append((person_name, []))
+
+        known_picture = face_recognition.load_image_file(image_path)
+        known_picture_encoded = face_recognition.face_encodings(known_picture)
+        if len(known_picture_encoded) == 0:
+            # no face found
+            print("WARNING: unable to add the file \"" + image_path + "\"")
+            if delete_image_without_faces:
+                os.remove(image_path)
+                print("WARNING: file deleted \"" + image_path + "\"")
+        else:
+            # the following line will append all the items of "known_picture_encoded" to "pic_list_encoded"
+            print("message: added new image file \"" + image_path + "\"")
+            self.known_faces_encoded[index_to_insert][1].extend(known_picture_encoded)
 
 
     def face_detection(self, picture_path, verify_all_faces = True, success_percentage = 0.6, distance_tolerance = 0.6):
