79e1a17de3f61abe320594033fa7ef6f3e56a670
Fenil Mehta <fenilgmehta@gmail.com>
2019-03-20 17:39:35 +0530

    client_RPi_face_training.py: imports updated for better interoperability on RaspberryPi and laptop, paths changed with variables from "path_initializer.py", image capturing method call updated and changed the loop condition from "done" -> "exit" for easy remembrance

--- src/client_RPi_face_training.py
+++ src/client_RPi_face_training.py
@@ -1,24 +1,46 @@
-# python 2
+# python 2/3
+
+print("=== import started (client_RPi_face_training) ===")
+
 from time import sleep
 import os
 
-import cam_pi
+try:
+    from cam_pi import Camera
+    is_raspberry_pi = True
+except ImportError:
+    from cam_laptop import Camera
+    is_raspberry_pi = False
+
+try:
+    input = raw_input
+except NameError:
+    pass
+
+import path_initializer
 import udp_transfer
 
-path = './face_testing/pictures_of_people_i_know/'
-camera_obj = cam_pi.Camera()
+print("=== import complete (client_RPi_face_training) ===")
 
-print("### Press enter to take new photo, write 'done' to stop.")
-ip_address = raw_input("Please enter the server IP address: ")
-port = raw_input("Please enter the server Port Number: ")
+#####################################################################################################################
+
+
+path_initializer.initialize_client_paths()
+camera_obj = Camera()
+
+print("\n")
+ip_address = input("Please enter the server IP address: ")
+port = input("Please enter the server Port Number: ")
 sender= udp_transfer.UdpTransfer(ip_address,port)
 
+print("\n\n### Press \"ENTER\" to take new photo, write \"exit\" to stop.")
+name = input("\n\nWhat's your name: ")
 msg=""
-name = raw_input("What's your name: ")
-while msg != "done":
-    img_counter = camera_obj.capture(path, name+'_')
-    sender.send_data("face_training",path+name+"_{}.png".format(img_counter))
+
+while msg != "exit":
+    img_path = camera_obj.capture_single_image(path_initializer.CLIENT_KNOWN_FACES_FOLDER, name+'_')[0]
+    sender.send_data("face_training", img_path, name)
     response=sender.recieve_data()
     print(response)
-    msg = raw_input()
+    msg = input()
 
