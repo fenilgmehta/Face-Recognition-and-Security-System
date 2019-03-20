ad9d202f9b7e80f4e5b420d1e259cc39e21f06b2
Fenil Mehta <fenilgmehta@gmail.com>
2019-03-20 16:35:18 +0530

    path_initializer.py: new module to hold all the paths in the form of variables to avoid ambiguity in multiple files and two new methods added to create the necessary directories if not present

---
+++ src/path_initializer.py
@@ -0,0 +1,22 @@
+# python 2/3
+import os
+
+SERVER_MAIN_DATA_FOLDER = "./z_face_testing"
+SERVER_KNOWN_FACES_FOLDER = SERVER_MAIN_DATA_FOLDER + "/" + "pictures_of_people_i_know"
+SERVER_UNKNOWN_FACES_FOLDER = SERVER_MAIN_DATA_FOLDER + "/" + "unknown_pictures"
+SERVER_PATHS = (SERVER_MAIN_DATA_FOLDER, SERVER_KNOWN_FACES_FOLDER, SERVER_UNKNOWN_FACES_FOLDER)
+
+CLIENT_MAIN_DATA_FOLDER = "./face_testing"
+CLIENT_KNOWN_FACES_FOLDER = CLIENT_MAIN_DATA_FOLDER + "/" + "pictures_of_people_i_know"
+CLIENT_UNKNOWN_FACES_FOLDER = CLIENT_MAIN_DATA_FOLDER + "/" + "unknown_pictures"
+CLIENT_PATHS = (CLIENT_MAIN_DATA_FOLDER, CLIENT_KNOWN_FACES_FOLDER, CLIENT_UNKNOWN_FACES_FOLDER)
+
+def initialize_server():
+    for i in SERVER_PATHS:
+        if not os.path.exists(i):
+            os.makedirs(i)
+
+def initialize_client():
+    for i in CLIENT_PATHS:
+        if not os.path.exists(i):
+            os.makedirs(i)
